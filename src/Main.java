1c0897df86671186556d50760dc44892f25cfd12
Ji <donji@microsoft.com>
2026-08-24 10:16:14 +0800

    Local in-editor commit test

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("wasup");
 
 
         for (int i = 1; i <= 5; i++) {
